e4d6b0237d86cd43a7291e5598f552582f2c7c55
Veerle van Leemput <info@hypebright.nl>
2024-03-16 11:29:58 +0100

    `jsonData` -> `data`

--- www/auth.js
+++ www/auth.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     $.get("https://hangman-shiny.azurewebsites.net/.auth/me", function (data) {
       
       // Extracting the user's name, preferred_username, and user_id
-      const name = jsonData[0].user_claims.find(claim => claim.typ === 'name').val;
-      const preferredUsername = jsonData[0].user_claims.find(claim => claim.typ === 'preferred_username').val;
-      const userId = jsonData[0].user_id;
+      const name = data[0].user_claims.find(claim => claim.typ === 'name').val;
+      const preferredUsername = data[0].user_claims.find(claim => claim.typ === 'preferred_username').val;
+      const userId = data[0].user_id;
       
       // Creating an object with extracted data
       const userData = {
