02f6df7a9abba9a9e362e82f9b8ac6060e35b265
Veerle van Leemput <info@hypebright.nl>
2024-03-16 17:01:59 +0100

    wait until shiny is connected

--- www/auth.js
+++ www/auth.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-    $.get("https://hangman-shiny.azurewebsites.net/.auth/me", function (data) {
-      
-      // Extracting the user's name, preferred_username, and user_id
-      const name = data[0].user_claims.find(claim => claim.typ === 'name').val;
-      const preferredUsername = data[0].user_claims.find(claim => claim.typ === 'preferred_username').val;
-      const userId = data[0].user_id;
-      
-      // Creating an object with extracted data
-      const userData = {
-          name: name,
-          preferred_username: preferredUsername,
-          user_id: userId
-      };
-      
-      // TEMP: for debugging purposes
-      console.log(userData)
+    // Wait until Shiny is fully initialized
+    $(document).on('shiny:connected', function(event) {
+        $.get("https://hangman-shiny.azurewebsites.net/.auth/me", function (data) {
+          
+          // Extracting the user's name, preferred_username, and user_id
+          const name = data[0].user_claims.find(claim => claim.typ === 'name').val;
+          const preferredUsername = data[0].user_claims.find(claim => claim.typ === 'preferred_username').val;
+          const userId = data[0].user_id;
+          
+          // Creating an object with extracted data
+          const userData = {
+              name: name,
+              preferred_username: preferredUsername,
+              user_id: userId
+          };
 
-      Shiny.setInputValue("AzureAuth", JSON.stringify(userData))
-      
-  }).fail(function () {
-      console.log("Error while trying to retrieve user data from Entra ID: defaulting to 'unauthorized'.");
-      Shiny.setInputValue("AzureAuth", "unauthorized");
-  });
+          Shiny.setInputValue("AzureAuth", userData);
+          
+      }).fail(function () {
+          console.log("Error retrieving user data from Entra ID: defaulting to 'unauthorized'.");
+          Shiny.setInputValue("AzureAuth", "unauthorized");
+      });
+    });
 });
